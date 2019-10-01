8e2a5b1a9720e8f54a5283714e3516e80c83aa17
glego <glego200323@gmail.com>
2019-10-01 17:38:21 +0300

    Lesson04 Reflection and JUnit (2)

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -1,9 +1,5 @@
 package ru.javawebinar.basejava.storage;
 
-public class ArrayStorageTest extends AbstractArrayStorageTest {
-
-    public ArrayStorageTest() {
-        super(new ArrayStorage());
-    }
+public class ArrayStorageTest extends AbstractArrayStorageTest{
 
 }
